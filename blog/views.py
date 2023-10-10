eb7b582d51730d1f6994134c7946c14fa8dfe719
Shubham879177 <shubhamapps87@gmail.com>
2023-10-10 21:37:46 +0530

    change

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+
+def blogpost(request,slug):
+    return render(request , 'blog/blogpost.html')
+    
+
+def bloghome(request):
+    return render(request , 'blog/bloghome.html')
+    
+
